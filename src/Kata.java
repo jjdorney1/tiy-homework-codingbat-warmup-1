78c01f141f167ff2804491ecc8dbaac044bf0255
jeffreydorney <jjdorney1@gmail.com>
2016-08-08 22:55:33 -0400

    completed all the parts

--- src/Kata.java
+++ src/Kata.java
@@ -215,7 +215,208 @@ public class Kata {
         }
     }
 
-    
+    // returns true if one temp is below zero and the other is above 100
+
+    public boolean icyHot(int temp1, int temp2) {
+        if( (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // returns true if a or b is between 10 and 20 (inclusive)
+
+    public boolean in1020(int a, int b) {
+        if(a >= 10 && a <= 20) {
+            return true;
+        } else if(b >= 10 && b <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // at least one of the variables needs to be between 13 and 19 (inclusive) to be true
+
+    public boolean hasTeen(int a, int b, int c) {
+        if(a >= 13 && a <= 19) {
+            return true;
+        } else if(b >= 13 && b <= 19) {
+            return true;
+        } else if(c >= 13 && c <= 19) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //
+
+    public boolean loneTeen(int a, int b) {
+        //return !((a >= 13 && a <= 19) && (b >= 13 && b <= 19));
+        boolean aTeen = (a >= 13) && (a <= 19);
+        boolean bTeen = (b >= 13) && (b <= 19);
+
+        /* code no longer needed but left in
+        if((a >= 13) && (a <= 19)) {
+          aTeen = true;
+        } else {
+          aTeen = false;
+        }
+
+        if((b >= 13) && (b <= 19)) {
+          bTeen = true;
+        } else {
+          bTeen = false;
+        }*/
+
+        return (aTeen && !bTeen) || (!aTeen && bTeen);
+
+    }
+
+    //
+
+    public String delDel(String str) {
+        //String delRemovedEnd;
+        //String firstLetter = str.charAt(0);
+
+        if( (str.length()>=4) && (str.substring(1, 4).equals("del")) ) {
+            return str.substring(0, 1) + str.substring(4);
+            //return str.charAt(0) + delRemovedEnd;
+
+        } else {
+            return str;
+        }
+
+    }
+
+    // return true if the first three letters start with #ix where # is any letter
+
+    public boolean mixStart(String str) {
+
+        if( (str.length() < 3)) return false;
+
+        String twoLetters = str.substring(1, 3);
+
+        return (twoLetters.equals("ix"));
+
+        /*} else if( (str.substring(1, 3) == "ix") ) {
+            return true;
+        } else {
+            return false;
+        }*/
+    }
+
+    //
+
+    public String startOz(String str) {
+        String letters = "";
+
+        // if the string is one letter it checks to see if it is also 'o'
+        if( (str.length() >= 1) && (str.charAt(0) == 'o') ) {
+            letters = letters + str.charAt(0);
+        }
+
+        //if the string is 2 letters long and the second letter is also a 'z'
+        if( (str.length() >= 2) && (str.charAt(1) == 'z') ) {
+            letters = letters + str.charAt(1);
+        }
+
+        return letters;
+    }
+
+    /* --- */
+
+    public int intMax(int a, int b, int c) {
+        int max;
+
+        if (a > b) {
+            max = a;
+
+        } else {
+            max = b;
+        }
+
+        if (c > max) {
+            max = c;
+        }
+
+        return max;
+    }
+
+    public int close10(int a, int b) {
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (bDiff < aDiff) {
+            return b;
+        }
+        return 0;
+    }
+
+    public boolean in3050(int a, int b) {
+        if (a >= 30 && a <= 40 && b >= 30 && b <= 40) {
+            return true;
+        }
+        if (a >= 40 && a <= 50 && b >= 40 && b <= 50) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public int max1020(int a, int b) {
+
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
+
+    public boolean stringE(String str) {
+        int count = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            if (str.charAt(i) == 'e') count++;
+
+        }
+
+        return (count >= 1 && count <= 3);
+    }
+
+    public boolean lastDigit(int a, int b) {
+
+        return(a % 10 == b % 10);
+    }
+
+    public String endUp(String str) {
+        if (str.length() <= 3) return str.toUpperCase();
+        int cut = str.length() - 3;
+        String front = str.substring(0, cut);
+        String back  = str.substring(cut);
+
+        return front + back.toUpperCase();
+    }
+
+    public String everyNth(String str, int n) {
+        String result = "";
+
+        for (int i=0; i<str.length(); i = i + n) {
+            result = result + str.charAt(i);
+        }
+
+        return result;
+    }
+
 
 
     /*
